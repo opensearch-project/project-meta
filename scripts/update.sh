28b9b0983188a8b8887aa8a191d3314fbd7bdaf2
Daniel (dB.) Doubrovkine <dblock@amazon.com>
2022-05-25 15:59:28 -0400

    Fix: gh command line uses a newer syntax, visibility=public. (#48)
    Signed-off-by: dblock <dblock@amazon.com>

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for repo in `gh repo list -L 100 --public opensearch-project | cut -f1 | cut -d/ -f2 | sort -f` 
+for repo in `gh repo list -L 100 --visibility=public opensearch-project | cut -f1 | cut -d/ -f2 | sort -f` 
 do
     if [ ! -d $repo ]; then
         git clone --depth 1 https://github.com/opensearch-project/$repo.git
     fi
 done
 
-for repo in `gh repo list -L 100 --public opensearch-project | cut -f1 | cut -d/ -f2 | sort -f` 
+for repo in `gh repo list -L 100 --visibility=public opensearch-project | cut -f1 | cut -d/ -f2 | sort -f` 
 do
     echo ⌛ Checking $repo ...
     meta project import $repo git@github.com:opensearch-project/$repo.git
